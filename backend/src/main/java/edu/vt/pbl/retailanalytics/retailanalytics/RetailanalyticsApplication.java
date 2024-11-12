f0d49bdc7b11871ee5d888e6e35974fb58be218b
Marrisa0912 <marissa1229587@gmail.com>
2024-11-12 01:39:24 -0500

    exclude the datasource from spring context to solve build issue

--- backend/src/main/java/edu/vt/pbl/retailanalytics/retailanalytics/RetailanalyticsApplication.java
+++ backend/src/main/java/edu/vt/pbl/retailanalytics/retailanalytics/RetailanalyticsApplication.java
@@ -2,8 +2,9 @@ package edu.vt.pbl.retailanalytics.retailanalytics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RetailanalyticsApplication {
 
 	public static void main(String[] args) {
