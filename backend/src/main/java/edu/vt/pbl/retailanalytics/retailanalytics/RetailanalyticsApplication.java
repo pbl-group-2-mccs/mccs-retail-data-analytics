17a1a9c13342893631c2cf6582e180a8de49f750
Marrisa0912 <marissa1229587@gmail.com>
2024-11-12 01:28:24 -0500

    inital setup

---
+++ backend/src/main/java/edu/vt/pbl/retailanalytics/retailanalytics/RetailanalyticsApplication.java
@@ -0,0 +1,13 @@
+package edu.vt.pbl.retailanalytics.retailanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailanalyticsApplication.class, args);
+	}
+
+}
