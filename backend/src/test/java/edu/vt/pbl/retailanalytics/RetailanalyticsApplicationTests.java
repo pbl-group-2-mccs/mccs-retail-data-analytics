48970f3590aa5ff81e71ec26a6cb98ade17b2056
Henrykim1111 <henrykim9319@gmail.com>
2024-12-14 01:24:25 -0500

    feat: link aws rds DB

--- backend/src/test/java/edu/vt/pbl/retailanalytics/RetailanalyticsApplicationTests.java
+++ backend/src/test/java/edu/vt/pbl/retailanalytics/RetailanalyticsApplicationTests.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 class RetailanalyticsApplicationTests {
 
 	@Container
-	static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.40")
+	static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.39")
 			.withDatabaseName("mydatabase")
 			.withUsername("myuser")
 			.withPassword("secret")
